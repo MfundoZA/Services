e5541686f693542fb3cea0a804013e039df29ec3
MfundoZA <20117271@vcconnect.co.za>
2021-07-28 13:12:37 +0200

    Debugged notification creation

--- app/src/main/java/com/mfundoza/services/MainActivity.java
+++ app/src/main/java/com/mfundoza/services/MainActivity.java
@@ -2,30 +2,63 @@ package com.mfundoza.services;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private final String APP_NAME = getText(R.string.app_name).toString();
-    private String Channel_ID = "main_channel";
+    private static final int CHANNEL_ID = 0;
+    NotificationManager notificationManager;
+    Notification notification;
+    Button notificationButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        notificationButton = (Button) findViewById(R.id.notificationButton);
+        notificationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createNotification();
+                createNotificationChannel();
+            }
+        });
     }
 
-    public void createNotification(View view) {
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, Channel_ID)
+    public void createNotification() {
+        notification = new NotificationCompat.Builder(this, String.valueOf(CHANNEL_ID))
                 .setSmallIcon(R.drawable.ic_launcher_foreground)
-                .setContentTitle(APP_NAME)
+                .setContentTitle("Services")
                 .setContentText("This is my first notification!")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT).build();
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Standard Channel";
+            String description = "The standard notification channel";
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(String.valueOf(CHANNEL_ID), name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        notificationManager.notify(CHANNEL_ID, notification);
     }
 }
